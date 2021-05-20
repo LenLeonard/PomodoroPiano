7917b8f897c99c70befb2df9f3499770026ee40d
unknown <lencompsci@gmail.com>
2021-05-20 11:03:36 -0230

    centered piano, added darkened keys on trigger.

--- tones.js
+++ tones.js
@@ -1,24 +1,40 @@
 import * as Tone from "https://cdn.skypack.dev/tone";
 
+
+
 export function playNote(note) {
   const synth = new Tone.Synth().toDestination();
   synth.triggerAttackRelease(note, "8n");
-} 
+  
+  /*Darkens the keys upon trigger.*/
+  
+  document.getElementById(note).classList.add("active");
+    setTimeout(function(){ document.getElementById(note).classList.remove("active");}, 120);
+  }
 
-document.getElementById("C").addEventListener("click", function(){ playNote('C4'); });
-document.getElementById("Db").addEventListener("click", function(){ playNote('Db4'); });
-document.getElementById("D").addEventListener("click", function(){ playNote('D4'); });
-document.getElementById("Eb").addEventListener("click", function(){ playNote('Eb4'); });
-document.getElementById("E").addEventListener("click", function(){ playNote('E4'); });
-document.getElementById("F").addEventListener("click", function(){ playNote('F4'); });
-document.getElementById("Gb").addEventListener("click", function(){ playNote('Gb4'); });
-document.getElementById("G").addEventListener("click", function(){ playNote('G4'); });
-document.getElementById("Ab").addEventListener("click", function(){ playNote('Ab4'); });
-document.getElementById("A").addEventListener("click", function(){ playNote('A4'); });
-document.getElementById("Bb").addEventListener("click", function(){ playNote('Bb4'); });
-document.getElementById("B").addEventListener("click", function(){ playNote('B4'); });
 
-const pianoKeys = ["a","w","s","e","d","f","t","g","y","h"]
+    
+    
+    
+
+
+
+
+document.getElementById("C4").addEventListener("click", function(){ playNote('C4'); });
+document.getElementById("Db4").addEventListener("click", function(){ playNote('Db4'); });
+document.getElementById("D4").addEventListener("click", function(){ playNote('D4'); });
+document.getElementById("Eb4").addEventListener("click", function(){ playNote('Eb4'); });
+document.getElementById("E4").addEventListener("click", function(){ playNote('E4'); });
+document.getElementById("F4").addEventListener("click", function(){ playNote('F4'); });
+document.getElementById("Gb4").addEventListener("click", function(){ playNote('Gb4'); });
+document.getElementById("G4").addEventListener("click", function(){ playNote('G4'); });
+document.getElementById("Ab4").addEventListener("click", function(){ playNote('Ab4'); });
+document.getElementById("A4").addEventListener("click", function(){ playNote('A4'); });
+document.getElementById("Bb4").addEventListener("click", function(){ playNote('Bb4'); });
+document.getElementById("B4").addEventListener("click", function(){ playNote('B4'); });
+document.getElementById("C5").addEventListener("click", function(){ playNote('C5'); });
+
+
 
 window.addEventListener('keydown', (event) => {
   const pianoKeys = ["a","w","s","e","d","f","t","g","y","h","u","j","k"];
@@ -89,7 +105,9 @@ window.addEventListener('keydown', (event) => {
     console.log(found);
     let keyNote = found.note;
     console.log(keyNote);
-    playNote(keyNote)
+    playNote(keyNote);
+    
+    
   }
     
 });
