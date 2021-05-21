5b6efe3d099ede5a5c51545a132aa56ecfff3d52
unknown <lencompsci@gmail.com>
2021-05-20 22:15:57 -0230

    Combined Pomo and piano pages

--- tones.js
+++ tones.js
@@ -1,5 +1,83 @@
 import * as Tone from "https://cdn.skypack.dev/tone";
 
+//Pomodoro JS
+
+let start = document.getElementById('start');
+let reset = document.getElementById('reset');
+let stop = document.getElementById('stop');
+
+let workMinutes = document.getElementById('w_minutes');
+let workSeconds = document.getElementById('w_seconds');
+
+let breakMinutes = document.getElementById('b_minutes');
+let breakSeconds = document.getElementById('b_seconds');
+
+let startTimer;
+
+start.addEventListener('click', function(){
+    if(startTimer === undefined){
+        startTimer = setInterval(timer, 1000)
+    }else {
+        alert("Timer is already running");
+    }
+})
+
+reset.addEventListener('click', function(){
+        workMinutes.innerText = 25;
+        workSeconds.innerText = "00";
+
+        breakMinutes.innerText = 5;
+        breakSeconds.innerText = "00";
+
+        document.getElementById('counter').innerText = 0;
+        stopInterval()
+        startTimer = undefined;
+
+})
+
+stop.addEventListener('click', function(){
+    stopInterval()
+    startTimer = undefined;
+
+})
+
+//Start Time Function
+function timer(){
+    //Work Timer Countdown
+    if (workSeconds.innerText != 0){
+        workSeconds.innerText--;
+    }else if(workMinutes.innerText != 0 && workSeconds.innerText == 0){
+        workSeconds.innerText = 59;
+        workMinutes.innerText--;
+    }
+    //Break Timer Countdown
+    if(workMinutes.innerText == 0 && workSeconds.innerText == 0){
+        if(breakSeconds.innerText != 0){
+            breakSeconds.innerText--;
+        } else if(breakMinutes.innerText !=0 && breakSeconds.innerText == 0){
+            breakSeconds.innerText = 59;
+            breakMinutes.innerText--;
+        }
+
+    }
+    //Increment Counter by one if one full cyle is completed
+    if(workMinutes.innerText == 0 && workSeconds.innerText == 0 && breakSeconds.innerText == 0){
+        workMinutes.innerText = 25;
+        workSeconds.innerText = "00";
+
+        breakMinutes.innerText = 5;
+        breakSeconds.innerText = "00";
+
+        document.getElementById('counter').innerText++;
+
+    }
+}
+//Stop Time Function
+function stopInterval(){
+    clearInterval(startTimer);
+}
+
+//Piano JS
 
 
 export function playNote(note) {
@@ -19,7 +97,7 @@ export function playNote(note) {
 
 
 
-
+//so you can click on a note for its tone
 document.getElementById("C4").addEventListener("click", function(){ playNote('C4'); });
 document.getElementById("Db4").addEventListener("click", function(){ playNote('Db4'); });
 document.getElementById("D4").addEventListener("click", function(){ playNote('D4'); });
@@ -37,7 +115,7 @@ document.getElementById("C5").addEventListener("click", function(){ playNote('C5
 
 
 window.addEventListener('keydown', (event) => {
-  const pianoKeys = ["a","w","s","e","d","f","t","g","y","h","u","j","k"];
+  const pianoKeys = ["a","w","s","e","d","f","t","g","y","h","u","j","k","o","l","p",";","'"];
   const keyMap = [
     {
        key: 'a',
@@ -94,6 +172,26 @@ window.addEventListener('keydown', (event) => {
 {
   key: 'k',
   note: 'C5'
+},
+{
+  key: 'o',
+  note: 'Db5'
+},
+{
+  key: 'l',
+  note: 'D5'
+},
+{
+  key: 'p',
+  note: 'Eb5'
+},
+{
+  key: ';',
+  note: 'E5'
+},
+{
+  key: "'",
+  note: 'F5'
 },
     
   ];
